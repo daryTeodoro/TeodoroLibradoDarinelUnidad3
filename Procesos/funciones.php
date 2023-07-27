6d4f712c71c5f17b31327f9663bc862be70f8ac3
Angel320 <92888451+Angel320@users.noreply.github.com>
2023-07-27 13:18:14 -0600

    Add files via upload

--- Procesos/funciones.php
+++ Procesos/funciones.php
@@ -1,21 +1,22 @@
 <?php
-include('conexion.php');
+include ("conexion.php");
+/*Funcion para consultar el correo*/
+function consulta_users($id){
+	$cnn = new Conexion();
+	$sql = $cnn->prepare("SELECT * FROM usuarios WHERE correo = :id");
+	$sql->bindParam(':id',$id);
+	$sql->execute();
+	$contador = $sql->rowCount();
 
-function consultaruser($Id){
-    $conexion= new conexion();
-    $usuario=$conexion->prepare('SELECT * FROM usuarios WHERE id_user = :id');
-    $usuario->bindParam(':id',$Id);
-    $usuario->execute();
-    $cu=$usuario->rowCount(); //Cuenta si existe el registro
-
-    if ($cu == 1) {
-        // Si se encontró un registro, devuelve los datos del usuario como un array asociativo
-        return $usuario->fetch(PDO::FETCH_ASSOC);
-    } else {
-        // Si no se encontró ningún registro, devuelve false
-        return false;
-    }
+	if ($contador==1) {
+		return $sql->fetch(PDO::FETCH_ASSOC);
+	} else {
+		return false;
+	}
+	
 }
+
+
 
 function consultarestado($Id){
     $conexion= new conexion();
@@ -33,4 +34,53 @@ function consultarestado($Id){
     }
 }
 
+
+
+/*Funcion para consulta el telefono*/
+function consulta_number($num){
+    $cnn = new Conexion();
+    $sql = $cnn->prepare("SELECT * FROM usuarios WHERE telefono = :num");
+    $sql->bindParam(':num',$num);
+    $sql->execute();
+    $contador = $sql->rowCount();
+
+    if ($contador==1) {
+        return $sql->fetch(PDO::FETCH_ASSOC);
+    } else {
+        return false;
+    }
+    
+}
+
+
+
+
+/*Funcion para hacer el registro de usuario*/
+function registrarUsuario($Nombre,$Correo,$Telefono,$Contrasena, $Rol) {
+    $Imagen = "imagen";
+    $conexion = new conexion(); 
+    $Insert = $conexion->prepare('INSERT INTO usuarios(imagen, nombre, correo, telefono, password_user, rol) VALUES (:imagen, :nombre, :correo, :telefono, :password_user, :rol)');
+    $Insert->bindParam(':imagen',$Imagen);
+    $Insert->bindParam(':nombre',$Nombre);
+    $Insert->bindParam(':correo',$Correo);
+    $Insert->bindParam(':telefono',$Telefono);
+    $Insert->bindParam(':password_user',$Contrasena);
+    $Insert->bindParam(':rol',$Rol);
+
+    $Insert->execute();
+
+    if ($Insert) {
+        return 1;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
 ?>
+
+
