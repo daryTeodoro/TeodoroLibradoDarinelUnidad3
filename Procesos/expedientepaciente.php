c54ac112afc4a855f4c8edf910d5e690c132b705
Angel320 <92888451+Angel320@users.noreply.github.com>
2023-08-09 11:07:33 -0600

    Add files via upload

--- Procesos/expedientepaciente.php
+++ Procesos/expedientepaciente.php
@@ -20,10 +20,11 @@
   }img{
     width: 100%;
     height: 92vh;
-    border-top: 2px solid #a9071d;
+    border-top: 4px solid #a9071d;
   }.Columnainfo{
-    display: grid;
-    grid-template-rows: 75% 25%;
+    overflow-y: auto;
+    border-top: 4px solid #a9071d;
+    animation: entrada 0.3s linear forwards;
   }.EspacioNav{
     display: grid;
     grid-template-columns: 20% 80%;
@@ -31,6 +32,37 @@
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+
+
+  .Tarea{
+    display: grid;
+    grid-template-columns: 20% 60% 20%;
+    transition: background 0.4s ease;
+  }.Tarea img{
+    width: 60px;
+    height: 60px;
+  }.Tarea .Col{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }.Col img{
+    border: none;
+  }
+
+  .Alineacion{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .download{
+    cursor: pointer;
+    font-size: 2rem;
   }
 </style>
 
@@ -43,15 +75,32 @@ $id = $_POST['id'];
 $verinfo = loguear($id);
 
   $conexion = new Conexion();
-  $info = $conexion->prepare("SELECT * FROM expedientes WHERE idpaciente = :idpa");
+  $info = $conexion->prepare("SELECT * FROM expedientes WHERE idpaciente = :idpa ORDER BY fecha DESC");
   $info->bindParam(':idpa',$id);
   $info->execute();
   $countinfo = $info->rowCount();
 
+  $infoextra = "";
+
   if ($countinfo > 0 ){
-    $infoextra = "helio";
+
+    while ($dataexpe=$info->fetch(PDO::FETCH_ASSOC)) {
+
+        $infoextra .= '<div class="p-3 Tarea">
+          <div class="Col"><img src="img/pdf.png"></div>
+          <div class="Col"><b class="Fuente-Fugaz">'.$dataexpe['nombre'].'</b></div>
+          <div class="Col">
+             <ion-icon name="cloud-download" onclick="download()" class="download"></ion-icon>
+          </div>
+        </div>';
+
+      }
+
   } else {
-    $infoextra = "No hay datos";
+    $infoextra .= '
+    <div class="Alineacion ListaAnalisis">
+      <h2 class="Fuente-Encode">No hay Registros</h2>
+    </div>';
   }
 
 echo '
@@ -61,6 +110,18 @@ echo '
 </div>
 <div class="Contenedor2Col">
   <div><img src="Procesos/'.$verinfo['imagen'].'"></div>
-  <div class="bg-dark Columnainfo">'.$infoextra.'</div>
+  <div class="bg-light Columnainfo Barra">
+     '.$infoextra.'
+  </div>
 </div>'
 ?>
+
+<script>
+        function download(){
+            var pdfUrl = "Procesos/pdf/CURP.pdf"; // Cambia esto por la URL de tu archivo PDF
+            var link = document.createElement("a");
+            link.href = pdfUrl;
+            link.download = "Analisis de Sangre.pdf"; // Cambia esto por el nombre deseado del archivo
+            link.click();
+        }
+    </script>
