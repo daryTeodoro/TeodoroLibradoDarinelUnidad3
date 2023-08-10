9b4e4f1f822b109934309618a50057794113dcab
gerardo7996 <126217775+gerardo7996@users.noreply.github.com>
2023-08-09 19:25:17 -0600

    Delete changestatus.php

--- Procesos/changestatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('conexion.php');
-$id = $_POST['id'];
-
-$conexion = new Conexion();
-$actst = $conexion->prepare('UPDATE tareas SET estatus = 0 WHERE id = :id'); 
-$actst->bindParam(':id',$id);
-$actst->execute();
-?>
